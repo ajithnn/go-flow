17a487fb1cb9653685d6dad65816f02af1290af4
ajithnn <ajithnn20@gmail.com>
2018-03-05 14:53:20 +0530

    rename Asset to Stage

--- flow/flow.go
+++ flow/flow.go
@@ -1,37 +1,37 @@
 package flow
 
 import (
-  scanner "github.com/ajithnn/sense"
-  "github.com/golang/glog"
-  "encoding/json"
-  "io/ioutil"
-  "path"
-  "strings"
-  "time"
-  "os"
+	"encoding/json"
+	scanner "github.com/ajithnn/sense"
+	"github.com/golang/glog"
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
+	"time"
 )
 
-type Asset interface {
-  Process(string, func())
+type Stage interface {
+	Process(string, func())
 }
 
-type notImplemented struct{
+type notImplemented struct {
 }
 
-func (n notImplemented) Process(filepath string,postProcess func()){
-  defer postProcess()
-  glog.V(2).Info("Type not found , unable to process",filepath)
-  return
+func (n notImplemented) Process(filepath string, postProcess func()) {
+	defer postProcess()
+	glog.V(2).Info("Type not found , unable to process", filepath)
+	return
 }
 
-type Flow struct{
-  ScanPath string
-  PipePath string
-  WhiteList string
-  Timeout float64
-  ScanTimeout time.Duration
-  TypeMap map[string]Asset
-  GetPrioritizedList func([]string)[]string
+type Flow struct {
+	ScanPath           string
+	PipePath           string
+	WhiteList          string
+	Timeout            float64
+	ScanTimeout        time.Duration
+	TypeMap            map[string]Stage
+	GetPrioritizedList func([]string) []string
 }
 
 var pipeChannels = make(map[string](chan struct{}))
@@ -41,87 +41,87 @@ var filePathList []string
 var FlowConfig Flow
 
 func Trigger(config Flow) {
-  FlowConfig = config
-  readConfigAndCreateChannels()
+	FlowConfig = config
+	readConfigAndCreateChannels()
 
-  ch := make(chan string)
-  w := scanner.FileScanner{FlowConfig.ScanPath, FlowConfig.Timeout, make(chan string), strings.Split(FlowConfig.WhiteList,","),&processList}
-  for {
-    go process(w.OutChannel, ch)
-    go w.Scan()
-    end := <-ch
-    if end == "__DONE" {
-      glog.V(2).Infof("Waiting for next scan")
-      glog.Flush()
-      time.Sleep(FlowConfig.ScanTimeout)
-    }
-  }
+	ch := make(chan string)
+	w := scanner.FileScanner{FlowConfig.ScanPath, FlowConfig.Timeout, make(chan string), strings.Split(FlowConfig.WhiteList, ","), &processList}
+	for {
+		go process(w.OutChannel, ch)
+		go w.Scan()
+		end := <-ch
+		if end == "__DONE" {
+			glog.V(2).Infof("Waiting for next scan")
+			glog.Flush()
+			time.Sleep(FlowConfig.ScanTimeout)
+		}
+	}
 }
 
 func process(c <-chan string, ch chan<- string) {
-  for {
-    filepath := <-c
-    if filepath == "__EOF" {
-      glog.V(2).Infof("End of current Scan will continue after wait.")
-      pushByPriority(filePathList)
-      ch <- "__DONE"
-    } else if len(filepath) > 0 {
-      filePathList = append(filePathList,filepath)
-    }
-  }
+	for {
+		filepath := <-c
+		if filepath == "__EOF" {
+			glog.V(2).Infof("End of current Scan will continue after wait.")
+			pushByPriority(filePathList)
+			ch <- "__DONE"
+		} else if len(filepath) > 0 {
+			filePathList = append(filePathList, filepath)
+		}
+	}
 }
 
-func pushByPriority(fileList []string){
-  filePathList = FlowConfig.GetPrioritizedList(fileList)
-  filepath := ""
-  for len(filePathList) > 0 {
-    filepath,filePathList = filePathList[0], filePathList[1:]
-    typeToProcess,typeName := getTypeFromFilePath(filepath)
-    actualProcess(typeToProcess, typeName, filepath)
-  }
+func pushByPriority(fileList []string) {
+	filePathList = FlowConfig.GetPrioritizedList(fileList)
+	filepath := ""
+	for len(filePathList) > 0 {
+		filepath, filePathList = filePathList[0], filePathList[1:]
+		typeToProcess, typeName := getTypeFromFilePath(filepath)
+		actualProcess(typeToProcess, typeName, filepath)
+	}
 }
 
-func readConfigAndCreateChannels(){
-  configFilePath := FlowConfig.PipePath
-  var tempPipe interface{}
-  commonChannel := make(chan struct{},1)
-  configFile, _ := ioutil.ReadFile(configFilePath)
-  json.Unmarshal(configFile, &tempPipe)
-  curPipe := tempPipe.(map[string]interface{})
-  for k,v := range curPipe{
-    tempType := v.(map[string]interface{})
-    glog.V(2).Infof("String %s Media values %f",k,tempType["capacity"].(float64))
-    if tempType["type"] == "separate"{
-      pipeChannels[k] = make(chan struct{},int(tempType["capacity"].(float64)))
-    }else{
-      pipeChannels[k] =commonChannel
-    }
-    channelTypes[k] = tempType["type"].(string)
-  }
+func readConfigAndCreateChannels() {
+	configFilePath := FlowConfig.PipePath
+	var tempPipe interface{}
+	commonChannel := make(chan struct{}, 1)
+	configFile, _ := ioutil.ReadFile(configFilePath)
+	json.Unmarshal(configFile, &tempPipe)
+	curPipe := tempPipe.(map[string]interface{})
+	for k, v := range curPipe {
+		tempType := v.(map[string]interface{})
+		glog.V(2).Infof("String %s Media values %f", k, tempType["capacity"].(float64))
+		if tempType["type"] == "separate" {
+			pipeChannels[k] = make(chan struct{}, int(tempType["capacity"].(float64)))
+		} else {
+			pipeChannels[k] = commonChannel
+		}
+		channelTypes[k] = tempType["type"].(string)
+	}
 }
 
-func getTypeFromFilePath(filepath string) (Asset,string) {
-  dir := path.Dir(filepath)
-  for k := range pipeChannels{
-    if strings.Contains(strings.ToLower(dir),strings.ToLower(k)){
-      return FlowConfig.TypeMap[k],k
-    }
-  }
-  return notImplemented{},""
+func getTypeFromFilePath(filepath string) (Stage, string) {
+	dir := path.Dir(filepath)
+	for k := range pipeChannels {
+		if strings.Contains(strings.ToLower(dir), strings.ToLower(k)) {
+			return FlowConfig.TypeMap[k], k
+		}
+	}
+	return notImplemented{}, ""
 }
 
-func actualProcess(processType Asset,typeName string, filepath string) {
-  select {
-  case pipeChannels[typeName] <- struct{}{}:
-    if _, err := os.Stat(filepath); !os.IsNotExist(err) {
-      processList[filepath] = true
-      go processType.Process(filepath,func(){
-        delete(processList,filepath)
-        <-pipeChannels[typeName]
-        glog.V(2).Infof("Released channel and cleared file hold.")
-      })
-    }
-  default:
-    glog.V(2).Infof("All channels blocked for type %s",typeName)
-  }
+func actualProcess(processType Stage, typeName string, filepath string) {
+	select {
+	case pipeChannels[typeName] <- struct{}{}:
+		if _, err := os.Stat(filepath); !os.IsNotExist(err) {
+			processList[filepath] = true
+			go processType.Process(filepath, func() {
+				delete(processList, filepath)
+				<-pipeChannels[typeName]
+				glog.V(2).Infof("Released channel and cleared file hold.")
+			})
+		}
+	default:
+		glog.V(2).Infof("All channels blocked for type %s", typeName)
+	}
 }
